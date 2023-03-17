5b3da5657645cb3bf3fe2c0b6688e3b1467cee58
hicham <hicha.taha@gmail.com>
2023-03-16 22:51:42 -0700

    init repo

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+
+var currentDateDisplay= document.getElementById('currentDay');
+console.log("working")
+
+
+const currentDate=dayjs().format() 
+console.log(currentDate);
+currentDateDisplay.textContent=currentDate
+
+console.log(currentDate)
+      console.log(currentDateDisplay)
+
+    
